df6645b4d35e38f6031baa42b77a700f027ad772
mozammilrahman52 <mozammilrahman52@gmail.com>
2024-01-29 00:02:40 +0530

    Second commit

---
+++ untitled/src/B.java
@@ -0,0 +1,22 @@
+public class B {
+    private static B b;
+    private B(){
+
+    }
+    public static B getB(){
+        if(b == null){
+            synchronized(B.class){
+                if(b == null){
+                    b = new B();
+                }
+            }
+        }
+        return b;
+    }
+    public static void main(String[] args){
+        B b1 = getB();
+        B b2 = getB();
+        System.out.println(b1);
+        System.out.println(b2);
+    }
+}
